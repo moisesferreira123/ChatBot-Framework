75f201d31f97fd9383c72797726bf3a651d22f5b
lucasaamorim <lucasapolonio@ufrn.edu.br>
2025-07-18 23:19:30 -0300

    remover excludes

--- chatbot/src/main/java/br/com/TrabalhoEngSoftwareFramework/framework/service/AiService.java
+++ chatbot/src/main/java/br/com/TrabalhoEngSoftwareFramework/framework/service/AiService.java
@@ -2,7 +2,6 @@ package br.com.TrabalhoEngSoftwareFramework.framework.service;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +9,11 @@ import reactor.core.publisher.Flux;
 
 @Service
 public class AiService {
-    @Autowired
-    private ChatClient chatClient;
+    private final ChatClient chatClient;
+
+    public AiService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
 
     public String getCompletion(Prompt prompt) {
         return chatClient.prompt(prompt).call().content();
